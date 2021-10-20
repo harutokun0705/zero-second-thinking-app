fdcb4e4a300f4d5c5db3861206577c6ef511b2a2
harutokun0705 <harutokun0705@yahoo.co.jp>
2021-10-20 23:13:23 +0900

    add:Supabaseの導入

--- next.config.js
+++ next.config.js
@@ -1,3 +1,7 @@
 module.exports = {
   reactStrictMode: true,
+  env: {
+    SUPABASE_URL: "https://qvrebnelluonakmusiho.supabase.co",
+    SUPABASE_KEY: "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJyb2xlIjoiYW5vbiIsImlhdCI6MTYzNDUzMDUzMCwiZXhwIjoxOTUwMTA2NTMwfQ.8_FeqFynHjNxehUKGU3U20u4Eyxr3hf3o_RbCSmrWFI",
+  },
 }
